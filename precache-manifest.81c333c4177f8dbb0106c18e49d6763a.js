630f26a3f39fd439f82fed5677ac424d26a7cd66
Senya Volkov <formobileprima@gmail.com>
2020-02-27 21:56:07 +0200

    Updates

---
+++ precache-manifest.81c333c4177f8dbb0106c18e49d6763a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa75fd743b7715e93deea28a05c80a41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db5b6619bac79a948ae2",
+    "url": "./static/css/main.b5d488a6.chunk.css"
+  },
+  {
+    "revision": "20c44e3ff0e76f4aaa61",
+    "url": "./static/js/2.1d55fe89.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "./static/js/2.1d55fe89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5b6619bac79a948ae2",
+    "url": "./static/js/main.c6cdceff.chunk.js"
+  },
+  {
+    "revision": "190d7602a3bebdff3f76",
+    "url": "./static/js/runtime-main.5b50cb44.js"
+  }
+]);
